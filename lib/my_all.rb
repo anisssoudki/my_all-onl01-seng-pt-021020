b5649e79cf957d1fa3e6189f4aea6410ea96cfbf
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 18:25:12 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
-  
+  block_return_values = []
  i = 0
   while i < collection.length
+  
   yield(collection[i]) #yields to the block at every itiration
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
