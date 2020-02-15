4c0f306bd87a09d273f087c15a6f00498a703920
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-15 18:25:33 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,7 @@ def my_all?(collection)
  i = 0
   while i < collection.length
   
-  lock_return_values << yield(collection[i]) #yields to the block at every itiration
+  Block_return_values << yield(collection[i]) #yields to the block at every itiration
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
 end
